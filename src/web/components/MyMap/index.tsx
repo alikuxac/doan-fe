3636fe92b2f3515d7abc3e11886a1b337e0c8835
Alikuxac <lexson270400@gmail.com>
2022-06-22 21:34:45 +0700

    feat(map): add marker, current pos circle

--- src/web/components/MyMap/index.tsx
+++ src/web/components/MyMap/index.tsx
@@ -1,9 +1,24 @@
 import React, { useEffect } from "react";
-import { GoogleMap, useJsApiLoader } from "@react-google-maps/api";
+import {
+  GoogleMap,
+  useJsApiLoader,
+  Circle,
+  Marker,
+} from "@react-google-maps/api";
+import { useJwtHook } from "../../hooks/useJwtHook";
+import { Favorite } from "@mui/icons-material";
+
+export interface FavoriteInterface {
+  name: string;
+  lat: number;
+  lng: number;
+  address: string;
+  createAt: number;
+}
 
 const containerStyle = {
   width: "100%",
-  height: "calc(100vh - 64px)",
+  height: "calc(100vh - 60px)",
 };
 
 const initCenter = {
@@ -15,6 +30,7 @@ function MyMap() {
   const { isLoaded } = useJsApiLoader({
     id: "google-map-script",
     googleMapsApiKey: "AIzaSyDyJoOIqGwx-cdSvp37X0KMRcyBA8SG3Ko",
+    libraries: ['geometry', 'places']
   });
 
   const [map, setMap] = React.useState(null);
@@ -35,16 +51,17 @@ function MyMap() {
   }, []);
 
   const getCurrentGeoLocation = () => {
-    //  ask for permission to use geolocation
-    navigator.permissions.query({ name: "geolocation" }).then((result) => {
-      if (result.state === "granted") {
-        // get the current position
-        navigator.geolocation.getCurrentPosition((position) => {
-          const { latitude, longitude } = position.coords;
-          setCenter({ lat: latitude, lng: longitude });
-        });
+    navigator.geolocation.getCurrentPosition(
+      (position) => {
+        const { latitude, longitude } = position.coords;
+        localStorage.setItem('lat', latitude.toString());
+        localStorage.setItem('lng', longitude.toString());
+        setCenter({ lat: latitude, lng: longitude });
+      },
+      (err) => {
+        console.error(err);
       }
-    });
+    );
   };
 
   console.log(center);
@@ -52,17 +69,66 @@ function MyMap() {
   useEffect(() => {
     // get current geolocation
     getCurrentGeoLocation();
-  }, [isLoaded, navigator]);
+  }, [navigator, setCenter]);
+
+  // Favorite
+  const [favoriteList, setFavoriteList] = React.useState<FavoriteInterface[]>(
+    []
+  );
+
+  const getFavorite = async () => {
+    const UserData = useJwtHook.getUserStorage();
+    const FavoriteStorage = useJwtHook.getUserFavorite();
+    if (FavoriteStorage) {
+      setFavoriteList(JSON.parse(FavoriteStorage));
+    } else {
+      useJwtHook.getFavorite(UserData?.id).then((res) => {
+      setFavoriteList(res.data.favorites);
+      useJwtHook.setUserFavorite(res.data.favorites);
+    });
+    }
+    
+  };
+
+  useEffect(() => {
+    getFavorite();
+  }, [setFavoriteList]);
+
+  const FavoriteMarkers = favoriteList.map((favorite) => {
+    return (
+      <Marker
+        position={{ lat: favorite.lat, lng: favorite.lng }}
+        label={favorite.name}
+      ></Marker>
+    );
+  });
 
   return isLoaded ? (
     <GoogleMap
       mapContainerStyle={containerStyle}
       center={center}
-      zoom={10}
+      zoom={7}
       onLoad={onLoad}
       onUnmount={onUnmount}
     >
       {/* Child components, such as markers, info windows, etc. */}
+      <Circle
+        center={center}
+        options={{
+          strokeColor: "blue",
+          strokeOpacity: 1,
+          strokeWeight: 2,
+          fillColor: "blue",
+          fillOpacity: 0.5,
+          clickable: false,
+          draggable: false,
+          editable: false,
+          visible: true,
+          zIndex: 2,
+        }}
+        radius={75}
+      />
+      {FavoriteMarkers}
       <></>
     </GoogleMap>
   ) : (
