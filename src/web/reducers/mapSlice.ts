8b37286e1c4e1006f4a9bf8031cd6c38f8780c64
Alikuxac <lexson270400@gmail.com>
2022-06-23 17:23:28 +0700

    fix(reducers): remove center default

--- src/web/reducers/mapSlice.ts
+++ src/web/reducers/mapSlice.ts
@@ -4,16 +4,10 @@ import { RootState } from '../store';
 const mapSlice = createSlice({
     name: "map",
     initialState: {
-        latCenter: 0,  // default lat
-        lngCenter: 0,  // default lng
-        lat: 0, // current lat
-        lng: 0, // current lng
+        lat: 0, // localStorage.getItem('lat'),
+        lng: 0 // localStorage.getItem('lng')
     },
     reducers: {
-        setCenter: (state, action) => {
-            state.latCenter = action.payload.latCenter;
-            state.lngCenter = action.payload.lngCenter;
-        },
         setCurrent: (state, action) => {
             state.lat = action.payload.lat;
             state.lng = action.payload.lng;
@@ -21,14 +15,7 @@ const mapSlice = createSlice({
     }
 });
 
-export const { setCenter, setCurrent } = mapSlice.actions;
-
-export const selectCenter = (state: RootState) => {
-    return {
-        latCenter: state.map.latCenter,
-        lngCenter: state.map.lngCenter
-    }
-};
+export const { setCurrent } = mapSlice.actions;
 
 export const selectCurrent = (state: RootState) => {
     return {
