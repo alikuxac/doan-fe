635fef5dbb2082582598b19eecc8e0c38433502b
Alikuxac <lexson270400@gmail.com>
2022-06-23 17:25:23 +0700

    chore(favorite): remove table width

--- src/web/components/Favorite.tsx
+++ src/web/components/Favorite.tsx
@@ -186,7 +186,7 @@ export default function Favorite() {
 
     return (
       <TableContainer component={Paper}>
-        <Table sx={{ minWidth: 500 }} aria-label="custom pagination table">
+        <Table aria-label="custom pagination table">
           <TableHead>
             <TableRow>
               <TableCell>Name</TableCell>
