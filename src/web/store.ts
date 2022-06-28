64e5ff9352dabfc9662cb60a26f81a8fb4c2f4e0
Alikuxac <lexson270400@gmail.com>
2022-06-28 20:39:21 +0700

    chore(store): add email

--- src/web/store.ts
+++ src/web/store.ts
@@ -1,10 +1,10 @@
 import { configureStore, ThunkAction, Action } from "@reduxjs/toolkit";
-import countReducer from "./reducers/countSlice";
+import emailReducer from "./reducers/emailSlice";
 import authReducer from "./reducers/authSlice";
 import mapReducer from './reducers/mapSlice';
 
 export const store = configureStore({
-  reducer: { count: countReducer, auth: authReducer, map: mapReducer },
+  reducer: { email: emailReducer, auth: authReducer, map: mapReducer },
 });
 
 export type AppDispatch = typeof store.dispatch;
